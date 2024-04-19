6e56eeac2725d406569b9abfcc0ca7fde9a98204
non-97 <nonmonaka07@gmail.com>
2024-04-19 12:04:37 +0900

    Initial commit

---
+++ test/saml-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SamlApp from '../lib/saml-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/saml-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SamlApp.SamlAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
